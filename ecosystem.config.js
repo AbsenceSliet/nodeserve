a288f015f68055efcb2cbca873b27b645eba9ff6
gitwork <1584123890@qq.com>
2019-06-05 18:30:19 +0800

    修改pm2 配置

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    apps: [{
+        name: 'API',
+        script: 'start.js',
+
+        args: 'one two',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+
+    deploy: {
+        production: {
+            user: 'Garen',
+            host: 'www.garener.com',
+            ref: 'origin/master',
+            repo: 'git@github.com:AbsenceSliet/nodeserve.git',
+            path: '/www/nodeServer/production',
+            'post-deploy': 'yarn  && pm2 startOrRestart ecosystem.config.js --env production',
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    }
+};
